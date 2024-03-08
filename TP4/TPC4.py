2623bb01b8c7a3fac90696fc6be7bfcce8db701f
Gerson Junior <ger_son_junior@live.com.pt>
2024-03-08 22:02:45 +0000

    TPC4

--- TP4/TPC4.py
+++ TP4/TPC4.py
@@ -1,56 +1,48 @@
 from ply import lex
 
-# Definição dos tokens
 tokens = (
     'SELECT', 'FROM', 'WHERE', 'DELIMITER1', 'DELIMITER2',
     'ATTRIBUTE', 'NUMBER', 'OPERATOR'
 )
 
-# Regras para tokens simples
 t_DELIMITER1 = r','
 t_DELIMITER2 = r';'
-
-# Regra para operadores
 t_OPERATOR = r'[<>]=?|='
 
 # Palavras reservadas (case insensitive)
 reserved_words = {'SELECT', 'FROM', 'WHERE'}
 
-# Regra para identificar palavras reservadas
 def t_ATTRIBUTE(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*|\*'
     if t.value.upper() in reserved_words:
         t.type = t.value.upper()
     return t
 
-# Regra para números
 def t_NUMBER(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
-# Regra para lidar com caracteres ignorados (por exemplo, espaços em branco)
 t_ignore = ' \t'
 
-# Regra para lidar com quebras de linha
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-# Regra para lidar com erros
 def t_error(t):
     print(f"Caractere ilegal '{t.value[0]}' na linha {t.lineno}")
     t.lexer.skip(1)
+    
+def main():
+    lexer = lex.lex()
+    codigo = "select id, nome, *, salario FROM empregados WHERE salario >= 820;"
+    lexer.input(codigo)
 
-# Construção do analisador léxico
-lexer = lex.lex()
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break
+        print(tok)
 
-# Exemplo de uso
-codigo = "select id, nome, *, salario FROM empregados WHERE salario >= 820;"
-lexer.input(codigo)
-
-while True:
-    tok = lexer.token()
-    if not tok:
-        break
-    print(tok)
+if __name__ == "__main__":
+    main()
